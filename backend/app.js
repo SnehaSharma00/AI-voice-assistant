b6a87cb8e39f5a17b35222868faa64afca52fdb8
SnehaSharma00 <sanuwkuku@gmail.com>
2026-09-02 23:11:43 +0530

    app.js edit

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 //app
 const express = require("express");
-const cors = require("cors");
+
 
 const app = express();
 
 // Middleware
-app.use(cors());
+
 app.use(express.json());
 
 // Test route
